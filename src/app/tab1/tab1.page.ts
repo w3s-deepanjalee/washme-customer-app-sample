2f601d74f2242650f2d657214fa74c6297d48b42
w3s-deepanjalee <deepanjalee@w3ssolutions.com>
2021-09-04 17:25:44 +0530

    Card item (Single)

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -3,10 +3,16 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
-  styleUrls: ['tab1.page.scss']
+  styleUrls: ['tab1.page.scss'],
 })
 export class Tab1Page {
-
   constructor() {}
 
+  // eslint-disable-next-line @typescript-eslint/member-ordering
+  slideOpts = {
+    slidesPerView: 4.5,
+    spaceBetween: 1,
+    centeredSlides: false,
+    loop: false,
+  };
 }
