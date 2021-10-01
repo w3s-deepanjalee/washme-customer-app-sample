42bedbbe1b0bb39713bc82177852dae450e8532d
Deepanjalee <deepa@cutomer-api.asia-southeast1-b.c.washme-295813.internal>
2021-10-01 06:25:56 +0000

    Profile tab form create

---
+++ src/app/components/components.module.ts
@@ -0,0 +1,8 @@
+import { NgModule } from "@angular/core";
+import { BackgroundComponent } from "./background/background.component";
+
+@NgModule({
+  declarations: [BackgroundComponent],
+  exports: [BackgroundComponent],
+})
+export class ComponentModule {}
