6de11905a830c95d016261fca30279d098ad4bbc
Deepanjalee <deepa@cutomer-api.asia-southeast1-b.c.washme-295813.internal>
2022-04-21 11:28:54 +0000

    empty order

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -3,9 +3,10 @@ import { BackgroundComponent } from "./background/background.component";
 import { FooterComponent } from "./footer/footer.component";
 import { AppHeaderComponent } from "./app-header/app-header.component";
 import { OrderButtonsComponent } from "./order-buttons/order-buttons.component";
+import { EmptyOrderComponent } from "./empty-order/empty-order.component";
 
 @NgModule({
-  declarations: [BackgroundComponent, FooterComponent,AppHeaderComponent,OrderButtonsComponent],
-  exports: [BackgroundComponent,FooterComponent,AppHeaderComponent,OrderButtonsComponent],
+  declarations: [BackgroundComponent, FooterComponent,AppHeaderComponent,OrderButtonsComponent,EmptyOrderComponent],
+  exports: [BackgroundComponent,FooterComponent,AppHeaderComponent,OrderButtonsComponent,EmptyOrderComponent],
 })
 export class ComponentModule {}
---
+++ src/app/components/empty-order/empty-order.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-empty-order',
+  templateUrl: './empty-order.component.html',
+  styleUrls: ['./empty-order.component.scss'],
+})
+export class EmptyOrderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
